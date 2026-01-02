e56eef895ecef4a0096e0ed6273299bef3546632
MICROHEM <MICROHEM@haryad.mshome.net>
2026-01-02 16:30:25 +0300

    Revert "Revert "Initial commit""
    This reverts commit 30f30edd8828b4b61161be9585f49222689297a9.

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
+
 /**
  *
  * @author Haryad
  */
 public class Main {
-    public static void main(String args[]){
-        
-        System.out.println("haryad"); 
+
+    public static void main(String args[]) {
+
+        System.out.println("haryad");
         String h = "haryad";
-        
+
         for (int i = 0; i < h.length(); i++) {
             System.out.println(h.charAt(i));
-            
-            
+
         }
-        
-        
-        
+
     }
 }
