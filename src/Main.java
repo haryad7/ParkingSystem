4e4deb3af085e531e3ebaed3a254fd409ab1893d
MICROHEM <MICROHEM@haryad.mshome.net>
2026-01-02 18:42:05 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,8 @@
 
-/**
- *
- * @author Haryad
- */
 public class Main {
 
     public static void main(String args[]) {
 
-        System.out.println("haryad");
-        String h = "haryad";
-
-        for (int i = 0; i < h.length(); i++) {
-            System.out.println(h.charAt(i));
-
-        }
-
     }
+
 }
