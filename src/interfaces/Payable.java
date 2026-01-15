a3ef16465171f33b0135c90997ea7a9f44221b05
PCnet <PCnet@DESKTOP-P57QAV8>
2026-01-15 12:10:18 -0800

    bo payable

--- src/interfaces/Payable.java
+++ src/interfaces/Payable.java
@@ -1,10 +1,26 @@
-
 package interfaces;
 
 /**
- *
- * @author 
+ * Interface defining the contract for payment processing.
+ * Implements strategy pattern for different payment methods.
+ * 
+ * @author Parking System Team
+ * @version 1.0
  */
 public interface Payable {
+    /**
+     * Processes a payment for the specified amount.
+     * 
+     * @param amount The amount to be paid
+     * @return true if payment was successful, false otherwise
+     */
+    boolean processPayment(double amount);
     
+    /**
+     * Calculates the parking fee based on duration.
+     * 
+     * @param hours The number of hours parked
+     * @return The total fee to be paid
+     */
+    double calculateFee(double hours);
 }
