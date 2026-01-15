0e942ad71630a70233f1e6c43ed2018f0c579402
PCnet <PCnet@DESKTOP-P57QAV8>
2026-01-15 12:10:03 -0800

    bo parkable

--- src/interfaces/Parkable.java
+++ src/interfaces/Parkable.java
@@ -1,10 +1,35 @@
 
 package interfaces;
 
+import models.ParkingSpot;
+
 /**
- *
- * @author 
+ * Interface defining the contract for objects that can be parked.
+ * Any vehicle that can occupy a parking spot must implement this interface.
+ * 
+ * @author Parking System Team
+ * @version 1.0
  */
 public interface Parkable {
+    /**
+     * Parks the vehicle in the specified parking spot.
+     * 
+     * @param spot The parking spot where the vehicle will be parked
+     * @return true if parking was successful, false otherwise
+     */
+    boolean park(ParkingSpot spot);
     
+    /**
+     * Removes the vehicle from its current parking spot.
+     * 
+     * @return true if the vehicle was successfully unparked, false otherwise
+     */
+    boolean unpark();
+    
+    /**
+     * Checks if the vehicle is currently parked.
+     * 
+     * @return true if the vehicle is parked, false otherwise
+     */
+    boolean isParked();
 }
